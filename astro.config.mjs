4d26b5a10b8029f2d7aae66cd1fd700c32418b4a
juten10x <122013366+juten10x@users.noreply.github.com>
2026-04-15 09:23:24 +0900

    https://docs.astro.build/ja/guides/deploy/github/

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://juten10x.github.io',
+	base: '/astro-tutorial',
+});
