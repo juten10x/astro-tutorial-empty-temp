d9233e223d241e56c168a7a5a926989a73cef001
juten10x <122013366+juten10x@users.noreply.github.com>
2026-04-15 10:27:34 +0900

    base修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://juten10x.github.io',
-	base: '/astro-tutorial',
+	base: '/astro-tutorial-empty-temp',
 });
