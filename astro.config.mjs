96fcbe32dd7fbb1ca89197283ab263a5c4163e0c
juten10x <122013366+juten10x@users.noreply.github.com>
2026-04-17 13:45:25 +0900

    いったん終了2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://juten10x.github.io',
-	base: '/astro-tutorial-empty-temp',
+  site: 'https://juten10x.github.io',
+  base: '/astro-tutorial-empty-temp',
+  integrations: [preact()],
 });
